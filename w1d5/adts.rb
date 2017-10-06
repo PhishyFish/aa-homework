5d565450c848329e83557a53836d0ca919c8bbbf
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-10-05 23:59:22 -0700

    Completed working Queue class

--- w1d5/adts.rb
+++ w1d5/adts.rb
@@ -17,13 +17,20 @@ class Stack
 end
 
 class Queue
+  def initialize
+    @queue = []
+  end
+
   def enqueue(el)
+    @queue << el
   end
 
   def dequeue
+    @queue.shift
   end
 
   def show
+    @queue
   end
 end
 
