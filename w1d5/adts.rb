2a9fcdd1490dabe96c3c94cd3b86c9d3c5d1b2ef
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-10-06 00:45:00 -0700

    Completed working Map class

--- w1d5/adts.rb
+++ w1d5/adts.rb
@@ -35,15 +35,29 @@ class Queue
 end
 
 class Map
+  def initialize
+    @map = []
+  end
+
   def assign(key, val)
+    if lookup(key)
+      @map.map { |pair| pair[1] = val if pair[0] == key }
+    else
+      @map << [key, val]
+    end
   end
 
   def lookup(key)
+    tuple = @map.select { |pair| pair[0] == key }
+    tuple.flatten[1]
   end
 
   def remove(key)
+    tuple = @map.select { |pair| pair[0] == key }
+    @map.delete(tuple.flatten)
   end
 
   def show
+    @map
   end
 end
