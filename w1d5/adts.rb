481e96d0b47806f8bc0c5d6331c1d08fce8294a1
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-10-05 23:46:08 -0700

    Added abstract data types assignment template

---
+++ w1d5/adts.rb
@@ -0,0 +1,38 @@
+class Stack
+  def initialize
+  end
+
+  def add(el)
+  end
+
+  def remove
+  end
+
+  def show
+  end
+end
+
+class Queue
+  def enqueue(el)
+  end
+
+  def dequeue
+  end
+
+  def show
+  end
+end
+
+class Map
+  def assign(key, val)
+  end
+
+  def lookup(key)
+  end
+
+  def remove(key)
+  end
+
+  def show
+  end
+end
