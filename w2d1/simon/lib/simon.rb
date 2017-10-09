127e4a1120dd8dc91401f0e043af7165866649ce
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-10-08 23:56:45 -0700

    Added command line playability, with error catching for incorrect input length

--- w2d1/simon/lib/simon.rb
+++ w2d1/simon/lib/simon.rb
@@ -30,10 +30,28 @@ class Simon
 
   def show_sequence
     add_random_color
+    puts @seq.join(", ")
+
+    sleep(2)
+    system("clear")
   end
 
   def require_sequence
+    puts "Simon says: (type colors separated by commas)"
+    guess = gets.downcase.chomp.split(", ")
 
+    if guess.count != @sequence_length
+      puts "Wrong number of colors; there are #{@sequence_length} in the sequence."
+      puts "Did you forget a comma?"
+
+      guess = gets.downcase.chomp.split(", ")
+    end
+
+    unless guess == @seq
+      puts "Wrong! You entered \"#{guess.join(', ')}\"."
+      puts "The correct sequence is \"#{@seq.join(', ')}\"."
+      @game_over = true
+    end
   end
 
   def add_random_color
@@ -41,11 +59,11 @@ class Simon
   end
 
   def round_success_message
-
+    "Great memory! Onto the next round."
   end
 
   def game_over_message
-
+    puts "Game over!"
   end
 
   def reset_game
@@ -53,4 +71,9 @@ class Simon
     @game_over = false
     @seq = []
   end
+end
+
+if $PROGRAM_NAME == __FILE__
+  puts "Let's play Simon! Repeat the color sequence in the correct order."
+  Simon.new.play
 end
