4827be2f3a7844896f0e81d5f8108a5d16aa0f88
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-10-13 05:49:02 -0700

    Completed LRU cache class definition

--- w2d5/lru_cache.rb
+++ w2d5/lru_cache.rb
@@ -1,20 +1,34 @@
 class LRUCache
-  def initialize
+  def initialize(size)
+    @size = size
+    @cache = []
   end
 
   def count
     # returns number of elements currently in cache
+    @cache.count
   end
 
   def add(el)
     # adds element to cache according to LRU principle
+    remove(el)
+    remove_lru
+    @cache << el
   end
 
   def show
     # shows the items in the cache, with the LRU item first
+    p @cache
+    nil
   end
 
   private
   # helper methods go here!
+  def remove(el)
+    @cache.delete(el) if @cache.include?(el)
+  end
 
+  def remove_lru
+    @cache.shift if count >= @size
+  end
 end
