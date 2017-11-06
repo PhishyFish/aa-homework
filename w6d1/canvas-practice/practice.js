b60a0eeff877acfb2dad2b2d40238502a33aa1cd
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-05 22:19:30 -0800

    Finished Phase II: Draw a rectangle

--- w6d1/canvas-practice/practice.js
+++ w6d1/canvas-practice/practice.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById('mycanvas');
+  canvas.width = 500;
+  canvas.height = 500;
 
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'rgb(150, 215, 225)';
+  ctx.fillRect(0, 0, 500, 500);
 });
