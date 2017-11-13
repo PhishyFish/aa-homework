bd6319263ba2ae705a23c7a934191344dc348057
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-13 01:39:07 -0800

    Added store, action creator, reducer

--- w7d1/dollar-store/frontend/reducer.js
+++ w7d1/dollar-store/frontend/reducer.js
@@ -3,8 +3,18 @@ const initialState = {
   rates: {}
 };
 
+const SWITCH_CURRENCY = "SWITCH_CURRENCY";
+
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  switch (action.type) {
+    case SWITCH_CURRENCY:
+      return {
+        baseCurrency: action.baseCurrency,
+        rates: action.rates
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
