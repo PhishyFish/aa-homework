8436dedac14d0b5f86fa7b6c10c6386a26bd5c2f
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-15 23:15:20 -0800

    Finished Phase III: navlinks

--- w7d4/rainbow-routes/components/rainbow.jsx
+++ w7d4/rainbow-routes/components/rainbow.jsx
@@ -16,13 +16,13 @@ class Rainbow extends React.Component {
       <div>
         <h1>Rainbow Router!</h1>
         {/* Your links should go here */}
-        <Link to="/red">Red only</Link>
-        <Link to="/red/orange">Add orange</Link>
-        <Link to="/red/yellow">Add yellow</Link>
-        <Link to="/green">Green only</Link>
-        <Link to="/blue">Blue only</Link>
-        <Link to="/blue/indigo">Add indigo</Link>
-        <Link to="/violet">Violet only</Link>
+        <NavLink exact to="/red">Red only</NavLink>
+        <NavLink to="/red/orange">Add orange</NavLink>
+        <NavLink to="/red/yellow">Add yellow</NavLink>
+        <NavLink to="/green">Green only</NavLink>
+        <NavLink exact to="/blue">Blue only</NavLink>
+        <NavLink to="/blue/indigo">Add indigo</NavLink>
+        <NavLink to="/violet">Violet only</NavLink>
 
         <div id="rainbow">
           {/* Your routes should go here */}
