d24ab47331785550e0b7485b5ed42361d023f84c
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-15 23:13:21 -0800

    Finished Phase II: links

--- w7d4/rainbow-routes/components/rainbow.jsx
+++ w7d4/rainbow-routes/components/rainbow.jsx
@@ -16,6 +16,13 @@ class Rainbow extends React.Component {
       <div>
         <h1>Rainbow Router!</h1>
         {/* Your links should go here */}
+        <Link to="/red">Red only</Link>
+        <Link to="/red/orange">Add orange</Link>
+        <Link to="/red/yellow">Add yellow</Link>
+        <Link to="/green">Green only</Link>
+        <Link to="/blue">Blue only</Link>
+        <Link to="/blue/indigo">Add indigo</Link>
+        <Link to="/violet">Violet only</Link>
 
         <div id="rainbow">
           {/* Your routes should go here */}
